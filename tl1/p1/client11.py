e2693136bd8b419008b31ec45690ed18a460bcb4
Conti Martin <tincho_conty_32@hotmail.com>
2021-09-03 16:16:08 -0300

    Agregado de todo punto 1

--- tl1/p1/client11.py
+++ tl1/p1/client11.py
@@ -2,25 +2,27 @@ from stub import Stub
 from mock_stub import MockStub
 from client import Cliente
 
-
 def main():
-
     # definicion de los stubs stub y mock_stub
     stub = Stub('localhost', 8090)
     mock_stub = MockStub('localhost', 8090)
 
-    # para emplear otro stub, se cambia el parametro 
-    # que se le brinda a la clase Cliente
     cliente = Cliente(stub)
 
     cliente.conectar()
 
-    cliente.enviar('un mensaje')
-   
-    mensaje = cliente.recibir()
-    print(mensaje)
+    while True:
+        print("Ingrese un mensaje para mandar al Servidor")
+        user_mensaje = input()
+
+        cliente.enviar(user_mensaje)
+
+        mensaje = cliente.recibir()
+        print(f"Respuesta: {mensaje}")
+
+        if user_mensaje == "x":
+            break
 
     cliente.desconectar()
-
 
 main()
--- tl1/p1/server11.py
+++ tl1/p1/server11.py
@@ -8,22 +8,22 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(server_address)
 server.listen()
 
-message = 'I am SERVER\n'
+message = 'I am Power SERVER\n'
 
 while True:
-	print('Server disponible!')
+	print('Servidor FULL disponible!')
 	connection, client_address = server.accept()
 	from_client = ''
 
 	while True:
 		data = connection.recv(4096)
-		if not data: break
+		if data.decode() == 'x':break
+		# Aqui acumula mensajes, y sale si llega una x
 		from_client += data.decode()
-
 		connection.send(message.encode('utf-8'))
 
 	print(from_client)
-
+	print('El Cliente pidio Salir!.')
+	connection.send(data)
 	connection.close()
-	print('cliente desconectado \n')
-
+	print('Cliente Desconectado \n')
